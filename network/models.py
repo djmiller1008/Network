a74c24ae6d7eaa1b70d89bda544a8e32a13d441d
David <djmiller1008@yahoo.com>
2022-10-04 23:30:03 +0800

    Add user followers table

--- network/models.py
+++ network/models.py
@@ -13,7 +13,10 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField(default=0)
 
-
+class UserFollowers(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="folowee")
+    follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers") 
+    
 
 
 
