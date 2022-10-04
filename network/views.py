727357a7853bcee3fc000a3b5056099608ee6407
David <djmiller1008@yahoo.com>
2022-10-04 23:25:48 +0800

    Implement post request functionality to the index route to create a post, render all posts on index route

--- network/views.py
+++ network/views.py
@@ -4,11 +4,22 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
+
 
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == 'POST':
+        user = request.user 
+        content = request.POST["post-content"]
+        post = Post(user=user, content=content)
+        post.save()
+
+    posts = Post.objects.all().order_by("-timestamp")
+
+    return render(request, "network/index.html", {
+        "posts": posts
+    })
 
 
 def login_view(request):
