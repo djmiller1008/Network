0ad91844fb801a4d02d91854d1547b3d8e360c24
David <djmiller1008@yahoo.com>
2022-10-04 23:26:07 +0800

    Add post model

--- network/models.py
+++ network/models.py
@@ -1,6 +1,19 @@
+from email.policy import default
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    content = models.CharField(max_length=150)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+
+
+
+
