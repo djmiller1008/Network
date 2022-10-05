98c96ba63dfda38598a06ca44e2ad07db97d8916
David <djmiller1008@yahoo.com>
2022-10-05 23:59:10 +0800

    Add profile and toggle follow routes

--- network/urls.py
+++ network/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("profile/<str:username>", views.profile, name="profile"),
+    path("toggle_follow", views.toggle_follow, name="toggle_follow") 
 ]
