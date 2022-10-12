29dd5f7d737159efaf27683d6b5d0a1dff43c189
David <djmiller1008@yahoo.com>
2022-10-12 10:45:10 +0800

    Register user, post, and userfollower to admin app

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, UserFollower, Post
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(UserFollower)
+admin.site.register(Post)
