45d42becf21baf8e35720e05596f53aa69895a30
David <djmiller1008@yahoo.com>
2022-10-16 21:40:38 +0800

    Add postlike to admin interface

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, UserFollower, Post
+from .models import User, UserFollower, Post, PostLike
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(UserFollower)
 admin.site.register(Post)
+admin.site.register(PostLike)
