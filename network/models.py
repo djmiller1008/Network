a35869d4000f19c99955b76bcc53c08737a2652a
David <djmiller1008@yahoo.com>
2022-10-16 21:41:01 +0800

    Rename likes to likes_number on Post model

--- network/models.py
+++ network/models.py
@@ -1,5 +1,4 @@
 from email.policy import default
-from enum import auto
 from tokenize import blank_re
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -13,6 +12,7 @@ class Post(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
     content = models.CharField(max_length=150)
     timestamp = models.DateTimeField(auto_now_add=True)
+    likes_number = models.PositiveIntegerField(default=0)
 
 class UserFollower(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
@@ -22,7 +22,6 @@ class PostLike(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="likes")
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="likes")
     timestamp = models.DateTimeField(auto_now_add=True)
-
 
 
 
