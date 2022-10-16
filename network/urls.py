96eea297c21404e5a6cf4290c642040379b60263
David <djmiller1008@yahoo.com>
2022-10-16 21:33:40 +0800

    Add routes for like and check_for_like

--- network/urls.py
+++ network/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path("profile/<str:username>", views.profile, name="profile"),
     path("toggle_follow", views.toggle_follow, name="toggle_follow"),
     path("following", views.following, name="following"),
-    path("edit_post/<int:post_id>", views.edit_post, name="edit_post") 
+    path("edit_post/<int:post_id>", views.edit_post, name="edit_post"),
+    path("like/<int:post_id>", views.like, name="like"),
+    path("check_for_like/<int:post_id>", views.check_for_like, name="check_for_like") 
 ]
