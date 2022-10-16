ac4eb3fbaa77f002e623a5dfe1cf946081b7675c
David <djmiller1008@yahoo.com>
2022-10-16 11:06:56 +0800

    Add functionality so user can toggle between editing a post and canceling the edit

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -6,13 +6,42 @@ document.addEventListener("DOMContentLoaded", () => {
         const index = editButton.dataset.index;
       
         editButton.addEventListener('click', () => {
-            const postContentTag = document.getElementById(index)
-            const textArea = document.createElement('textarea');
-           
-            textArea.innerHTML = postContentTag.innerHTML
-    
-            postContentTag.parentNode.replaceChild(textArea, postContentTag)
-            editButton.innerHTML = 'Save'
+            if (editButton.innerHTML === 'Edit') {
+
+            
+                const postContentTag = document.getElementById(`${index} post-content`);
+                const textArea = document.createElement('textarea');
+            
+                textArea.innerHTML = postContentTag.innerHTML;
+                textArea.id = `${index} textarea`;
+                textArea.name = 'post-content';
+                textArea.className = 'post-content-textarea';
+                postContentTag.style.display = 'none';
+
+                const form = document.getElementById(`${index} form`)
+                form.appendChild(textArea);
+
+                const submit = document.createElement('input');
+                submit.type = 'submit';
+                submit.value = 'Enter';
+                submit.className = 'btn btn-primary submit-button';
+                submit.id = `${index} submit-button`;
+                form.appendChild(submit);
+
+                editButton.innerHTML = 'Cancel';
+            } else {
+                const textArea = document.getElementById(`${index} textarea`);
+                textArea.innerHTML = '';
+                textArea.remove();
+
+                const postContentTag = document.getElementById(`${index} post-content`);
+                postContentTag.style.display = 'block';
+                
+                const submitButton = document.getElementById(`${index} submit-button`);
+                submitButton.remove()
+                editButton.innerHTML = 'Edit';
+            }
+
         })
 
     })
